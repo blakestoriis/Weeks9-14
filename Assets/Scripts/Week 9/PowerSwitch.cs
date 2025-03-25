f2156d069260ae29e7cdcf2f6b2f2648c10b1254
shodyl <shodyl@OA-S141-15>
2025-03-25 17:28:42 -0400

    started power switch

---
+++ Assets/Scripts/Week 9/PowerSwitch.cs
@@ -0,0 +1,28 @@
+using Microsoft.Unity.VisualStudio.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerSwitch : MonoBehaviour
+{
+
+    public Image Light;
+
+    public Sprite on;
+    public Sprite off;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
